857aac7c5406e5a51db7293a43c34b68c6dbcfff
Yrka <varnack30@gmail.com>
2024-01-27 12:30:07 +0300

    Сделал подгрузку изображение и прогресс бар, но оно все не работает

--- app/src/main/java/com/example/yourbarcode/MainActivity.java
+++ app/src/main/java/com/example/yourbarcode/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.yourbarcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,20 +33,41 @@ public class MainActivity extends AppCompatActivity {
         createBarcodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createBarcodeDialog();
+                if(barcodeTextEt.getText().length() == 0)
+                    Toast.makeText(MainActivity.this, "Введите текст!", Toast.LENGTH_LONG).show();
+                else
+                    createBarcodeDialog(barcodeTextEt.getText().toString());
             }
         });
 
     }
 
-    private void createBarcodeDialog(){
+    private void createBarcodeDialog(String text){
         BottomSheetDialog dialog = new BottomSheetDialog(this);
         dialog.setContentView(R.layout.barcode_fragment_layout);
 
+        ImageView barcodeIv = dialog.findViewById(R.id.barcode_iv);
+        ProgressBar progressBar = dialog.findViewById(R.id.progress_bar);
+
         WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+
+        float userBrightness = layoutParams.screenBrightness;
+
         layoutParams.screenBrightness = 1;
         getWindow().setAttributes(layoutParams);
 
+        Picasso.get().load("https://bwipjs-api.metafloor.com/?bcid=code128&text="+text+"&alttext=Your+barcode+app").into(barcodeIv);
+        progressBar.setVisibility(View.INVISIBLE);
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                layoutParams.screenBrightness = userBrightness;
+                getWindow().setAttributes(layoutParams);
+            }
+        });
+
         dialog.show();
+
+
     }
 }
