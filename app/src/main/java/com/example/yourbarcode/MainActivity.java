8630c700cbaf509b4f5c5c14450cbce56d18608f
Yrka <varnack30@gmail.com>
2024-01-27 12:34:45 +0300

    Задокументировал весь код

--- app/src/main/java/com/example/yourbarcode/MainActivity.java
+++ app/src/main/java/com/example/yourbarcode/MainActivity.java
@@ -19,6 +19,7 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Объявление переменных для эл. разметки
     Button createBarcodeBtn;
     EditText barcodeTextEt;
 
@@ -27,9 +28,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Инициализация переменных для эл. разметки
         createBarcodeBtn = findViewById(R.id.create_barcode_button);
         barcodeTextEt = findViewById(R.id.barcode_text_et);
 
+        //Вызов диалога при нажатии на кнопку
         createBarcodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,21 +46,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createBarcodeDialog(String text){
+        //Создание диалога и назначение экрана разметки
         BottomSheetDialog dialog = new BottomSheetDialog(this);
         dialog.setContentView(R.layout.barcode_fragment_layout);
 
+        //Инициализация переменных для эл. разметки диалога
         ImageView barcodeIv = dialog.findViewById(R.id.barcode_iv);
         ProgressBar progressBar = dialog.findViewById(R.id.progress_bar);
 
+        //Получение доступа к параметрам экрана пользователя
         WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
 
+        //Сохранение яркости пользователя
         float userBrightness = layoutParams.screenBrightness;
 
+        //Включение яркости на максимум
         layoutParams.screenBrightness = 1;
+        //Обновление параметров экрана
         getWindow().setAttributes(layoutParams);
 
+        //Подгрузка изображения и уборка прогресс бара
         Picasso.get().load("https://bwipjs-api.metafloor.com/?bcid=code128&text="+text+"&alttext=Your+barcode+app").into(barcodeIv);
         progressBar.setVisibility(View.INVISIBLE);
+
+        //Возвращение пользовательской яркости экрана при закрытии диалога
         dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialogInterface) {
@@ -66,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Показ диалога
         dialog.show();
 
 
